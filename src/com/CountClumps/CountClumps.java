cae116afe9869cf30be632da84934972af1d2cd4
vonrold <von.pamintuan@gmail.com>
2014-08-15 16:23:18 +0800

    Signed-off-by: vonrold <von.pamintuan@gmail.com>

---
+++ src/com/CountClumps/CountClumps.java
@@ -0,0 +1,11 @@
+package com.CountClumps;
+import java.util.*;
+import java.io.*;
+
+public class CountClumps {
+	
+	public static void main(String[] args){
+		
+	 System.out.println("helloworld!");		
+	}
+}
